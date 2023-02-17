15f1b7801eb49b2c6699a98e88ada6c94d9b7d85
jadams42392 <125307914+jadams42392@users.noreply.github.com>
2023-02-17 10:09:19 -0600

    Update streamlit_app.py
    changed variable in GET request to fruit_choice, which gets input from the user via Streamlit textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-#New Section to display fruityvice api response
+#New Section to display fruityvice api response. Add Text Entry Box and send the input to Fruityvice as part of the API call
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/" + "kiwi") 
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/" + fruit_choice) 
 
 # "normalize" the json 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
