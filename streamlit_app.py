1ff89023d855c4aa79742f6f0796ee1626f9c761
jadams42392 <125307914+jadams42392@users.noreply.github.com>
2023-02-22 07:57:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,6 @@ streamlit.dataframe(my_data_rows)
 # Add Text Entry Box and send the input to Fruityvice as part of the API call
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
